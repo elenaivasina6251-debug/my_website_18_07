d3e3d92c86cc8ce76b96b045657e454c2287e941
Elena Ivashina <elenaivasina6251@gmail.com>
2026-08-14 11:21:22 +0300

    Install Yandex Metrika and form goal

---
+++ assets/metrika.js
@@ -0,0 +1,17 @@
+(function (m, e, t, r, i, k, a) {
+  m[i] = m[i] || function () {
+    (m[i].a = m[i].a || []).push(arguments);
+  };
+  m[i].l = 1 * new Date();
+  k = e.createElement(t);
+  a = e.getElementsByTagName(t)[0];
+  k.async = true;
+  k.src = r;
+  a.parentNode.insertBefore(k, a);
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+ym(111595864, 'init', {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true
+});
